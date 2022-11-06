7d5292de61605aef440142fd7c23c4e08b629d75
Alberto <abuenocr21dw@ikzubirimanteo.com>
2022-11-06 18:37:23 +0100

    ReStructured blades. Now we have a template, and partials

--- Tasks/resources/views/tasks/index.blade.php
+++ Tasks/resources/views/tasks/index.blade.php
@@ -1,30 +1,14 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TasksManager</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <style>
-        body{
-            margin: 0;
-            padding: 0;
-            font-family: sans-serif;
-        }
-        .color-container{
-            width: 16px;
-            height: 16px;
-            display: inline-block;
-            border-radius: 4px;
-        }
-        a{
-            text-decoration: none;
-        }
-    </style>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('app')
+
+@section('content')
+    <div class="container w-25 border p-4 mt-5">
+        <form>
+            <div class="form-group">
+                <label for="taskName">New Task</label>
+                <input type="text" class="form-control" id="taskInput" aria-describedby="emailHelp" placeholder="Task name" name="taskName">
+                <small id="taskHelper" class="form-text text-muted">Done is better than perfect!</small>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+@endsection
--- Tasks/routes/web.php
+++ Tasks/routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
+});
+Route::get('/tasks', function () {
+    return view('tasks.index');
 });

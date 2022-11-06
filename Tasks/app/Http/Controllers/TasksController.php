27990789d89b813c27ef246c042915620e7d23b2
Alberto <abuenocr21dw@ikzubirimanteo.com>
2022-11-06 19:26:46 +0100

    TasksController@store created

--- Tasks/app/Http/Controllers/TasksController.php
+++ Tasks/app/Http/Controllers/TasksController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TasksController extends Controller
 {
     //
+    function index(){
+
+    }
+
+    function store(Requet $request){
+        $request->validate([
+            'name' => ' required'
+        ]);
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save;
+
+        return redirect()->route('tasks')->with('success','Task created successful');
+    }
+
+    function update(){
+
+    }
+    
+    function destroy(){
+
+    }
+
+    function edit (){
+
+    }
 }
